f43a65e5fc81a592640c4e8bd2a8f7be78a10208
Michael Clifford <cliffom@gmail.com>
2024-02-16 10:19:48 -0800

    Changed some parameter ordering, formatting

--- arduino-weather-clock.ino
+++ arduino-weather-clock.ino
@@ -19,13 +19,12 @@ Display lcd(12, 11, 10, 9, 8, 7);
 Climate climate(2, "Office");
 Datetime datetime;
 
-// Initialize our comfort indicator on pins 4, 5, and 6
-ComfortIndicator comfortIndicator(4, 5, 6);
+// Initialize our comfort indicator on the provided pins (R,G,B)
+ComfortIndicator comfortIndicator(6, 5, 4);
 
 auto timer = timer_create_default();
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
 
   // uncomment below to set the time
@@ -41,41 +40,37 @@ void setup()
   timer.every(60000, updateClimateAndSendToSerial);
 }
 
-void loop()
-{
+void loop() {
   timer.tick();
 }
 
 // Refreshes the visual outputs, including:
 // - LCD
 // - Comfort Indicator
-void updateVisuals()
-{
+void updateVisuals() {
   if (climate.error())
     comfortIndicator.displayWarning();
   else
     comfortIndicator.display(climate.getTemperature());
 
   lcd.updateDisplay(
-      datetime.dateToString(),
-      datetime.timeToString() + " " + climate.toString());
+    datetime.dateToString(),
+    datetime.timeToString() + " " + climate.toString());
 }
 
 // Sends data to the serial console in the format
 // error:0,status:ok,location:office,temperature:25,humidity:16
-void sendDataToSerial()
-{
+void sendDataToSerial() {
   String temperature = String(climate.getTemperature(), DEC);
   String humidity = String(climate.getHumidity(), DEC);
   String error = String(climate.error(), DEC);
-  
+
   Serial.println(
-      "error:" + error + ",status:" + climate.status() + ",location:" + climate.getLocation() + ",temperature:" + temperature + ",humidity:" + humidity);
+    "error:" + error + ",status:" + climate.status() + ",location:" + climate.getLocation() + ",temperature:" + temperature + ",humidity:" + humidity);
 }
 
 // Helper function to update the datetime and refresh the display
-bool updateDateTimeAndDisplay(void *)
-{
+bool updateDateTimeAndDisplay(void *) {
   datetime.refresh();
   if (datetime.seconds() == 0)
     updateVisuals();
@@ -84,8 +79,7 @@ bool updateDateTimeAndDisplay(void *)
 }
 
 // Helper function to update the climate and send the data to serial
-bool updateClimateAndSendToSerial(void *)
-{
+bool updateClimateAndSendToSerial(void *) {
   climate.update();
   sendDataToSerial();
 
--- comfort_indicator.h
+++ comfort_indicator.h
@@ -1,50 +1,45 @@
-class ComfortIndicator
-{
+class ComfortIndicator {
 private:
-    int BlueLED;
-    int GreenLED;
-    int RedLED;
-    const int Intensity = 64;
+  int RedLED;
+  int GreenLED;
+  int BlueLED;
+  const int Intensity = 64;
 
 public:
-    // Constructor that tells the indicator what pins to use for LEDs.
-    ComfortIndicator(int bluePin, int greenPin, int redPin)
-    {
-        BlueLED = bluePin;
-        GreenLED = greenPin;
-        RedLED = redPin;
+  // Constructor that tells the indicator what pins to use for LEDs.
+  ComfortIndicator(int redPin, int greenPin, int bluePin) {
+    RedLED = redPin;
+    GreenLED = greenPin;
+    BlueLED = bluePin;
 
-        reset();
-    }
+    reset();
+  }
 
-    // Sets all pins to LOW/no signal
-    void reset()
-    {
-        analogWrite(BlueLED, 0);
-        analogWrite(GreenLED, 0);
-        analogWrite(RedLED, 0);
-    }
+  // Sets all pins to LOW/no signal
+  void reset() {
+    analogWrite(RedLED, 0);
+    analogWrite(GreenLED, 0);
+    analogWrite(BlueLED, 0);
+  }
 
-    // Sets which LED to enable based on a rough temperate range of:
-    // - Hot when temperature >=30
-    // - Normal when temperature < 30 but >= 21
-    // - Cold when temperature <21
-    void display(int temperature)
-    {
-        reset();
-        if (temperature >= 25)
-            analogWrite(RedLED, Intensity);
-        else if (temperature >= 21)
-            analogWrite(GreenLED, Intensity);
-        else
-            analogWrite(BlueLED, Intensity);
-    }
+  // Sets which LED to enable based on a rough temperate range of:
+  // - Hot when temperature >= 25
+  // - Normal when temperature < 25 but >= 21
+  // - Cold when temperature < 21
+  void display(int temperature) {
+    reset();
+    if (temperature >= 25)
+      analogWrite(RedLED, Intensity);
+    else if (temperature >= 21)
+      analogWrite(GreenLED, Intensity);
+    else
+      analogWrite(BlueLED, Intensity);
+  }
 
-    // Turns the LED yellow to indicate that there was an issue
-    void displayWarning()
-    {
-        reset();
-        analogWrite(RedLED, Intensity);
-        analogWrite(GreenLED, Intensity);
-    }
+  // Turns the LED yellow to indicate that there was an issue
+  void displayWarning() {
+    reset();
+    analogWrite(RedLED, Intensity);
+    analogWrite(GreenLED, Intensity);
+  }
 };
